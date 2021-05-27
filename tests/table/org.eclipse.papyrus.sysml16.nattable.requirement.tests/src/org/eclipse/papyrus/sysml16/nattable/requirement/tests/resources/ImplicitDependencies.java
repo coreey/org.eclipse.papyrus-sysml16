7a692f9618986113ea21baf4858cf1fd0b454e36
Vincent Lorenzo <vincent.lorenzo@cea.fr>
2021-05-27 14:51:41 +0200

    Bug 573917: [SysML 1.6][Table] Fix Table Junit Tests
    Signed-off-by: Vincent Lorenzo <vincent.lorenzo@cea.fr>
    Change-Id: Ie25da988d948205681158689dd3de7b3d6ae16d0

---
+++ tests/table/org.eclipse.papyrus.sysml16.nattable.requirement.tests/src/org/eclipse/papyrus/sysml16/nattable/requirement/tests/resources/ImplicitDependencies.java
@@ -0,0 +1,38 @@
+/*****************************************************************************
+ * Copyright (c) 2021 CEA LIST and others.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Vincent Lorenzo (CEA LIST) <vincent.lorenzo@cea.fr> - Initial API and implementation
+ *   
+ *****************************************************************************/
+
+package org.eclipse.papyrus.sysml16.nattable.requirement.tests.resources;
+
+/**
+ * This class allows to explicit dependencies required for the JUnit tests
+ *
+ */
+public final class ImplicitDependencies {
+
+	@SuppressWarnings({ "restriction", "unused" })
+	private org.eclipse.ocl.internal.OCLPlugin.Implementation tmp1;
+	@SuppressWarnings("unused")
+	private org.eclipse.ocl.pivot.util.PivotPlugin.Implementation tmp2;
+	@SuppressWarnings("unused")
+	private org.eclipse.ocl.xtext.essentialocl.utilities.EssentialOCLPlugin.Implementation tmp3;
+	@SuppressWarnings("unused")
+	private org.eclipse.emf.validation.ocl.AbstractOCLModelConstraint<?, ?, ?, ?> tmp4;
+	@SuppressWarnings({ "restriction", "unused" })
+	private org.eclipse.ocl.ecore.internal.OCLEcorePlugin.Implementation tmp5;
+	@SuppressWarnings("unused")
+	private org.eclipse.ocl.xtext.essentialocl.ui.internal.EssentialOCLActivator tmp6;
+	@SuppressWarnings("unused")
+	private org.eclipse.ocl.xtext.completeocl.utilities.CompleteOCLPlugin.Implementation tmp7;
+}
